903a8b9a93ba90fd26d0d6c3eebd7aa7be79cc7d
CunningLobster <cherepansky.yan@yandex.ru>
2021-12-17 22:22:27 +0300

    Try create moving

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    Vector3 offset;
+
+    private void Awake()
+    {
+        offset = target.position - transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = target.position - offset;
+    }
+}
