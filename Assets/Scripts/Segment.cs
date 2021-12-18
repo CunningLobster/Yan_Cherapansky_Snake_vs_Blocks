98793ab4bb28ce7b27de0ff48f74e351283a6060
CunningLobster <cherepansky.yan@yandex.ru>
2021-12-18 23:49:17 +0300

    Try to record transforms

--- Assets/Scripts/Segment.cs
+++ Assets/Scripts/Segment.cs
@@ -15,6 +15,12 @@ namespace Assets.Scripts
         [SerializeField] Segment head;
 
         public bool isHead;
+        public float k = 0;
+
+        [Header("Adjusting")]
+        [Range(0, 50)] public float minK = 0;
+        [Range(0, 50)] public float maxK = .5f;
+        [Range(0, 2)] public float toNodeDistance = 1f;
 
         private void Awake()
         {
@@ -24,14 +30,18 @@ namespace Assets.Scripts
         private void Update()
         {
 
-            movingVector = new Vector3(0, 0, movingSpeed) * Time.deltaTime;
+            //movingVector = new Vector3(0, 0, movingSpeed) * Time.deltaTime;
             if (isHead)
             {
+                movingVector.z = movingSpeed * Time.deltaTime;
                 movingVector.x = FindObjectOfType<InputProvider>().XSpeed * Time.deltaTime;
             }
             else
             {
-                movingVector.x = Mathf.Lerp(0, head.movingVector.x, .9f);
+                movingVector.z = node.position.z - transform.position.z;
+                k = Mathf.Abs(node.position.x - transform.position.x) > toNodeDistance ? maxK : minK;
+                movingVector.x = (node.position.x - transform.position.x) * k * Time.deltaTime;
+                print(node.position.x - transform.position.x);
             }
             characterController.Move(movingVector);
 
