c9a001c67769f21aa38f750132e440d66dd0650f
CunningLobster <cherepansky.yan@yandex.ru>
2021-12-19 20:12:42 +0300

    Demo game moment

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Block : MonoBehaviour
 {
@@ -8,9 +9,35 @@ public class Block : MonoBehaviour
     [SerializeField] int value;
 
     [Range(0, .5f), SerializeField] float destroyingFrequency = .1f;
+    Color color;
+    Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+        color = new Color(1 - value /25f , material.color.g, value/ 25f);
+        GetComponent<Renderer>().material.color = color;
+
+    }
 
     private void Update()
     {
+        if (Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            value--;
+            color = new Color(1 - value / 25f, material.color.g, value/ 25f);
+            GetComponent<Renderer>().material.color = color;
+        }
+
+        if (Keyboard.current.vKey.wasPressedThisFrame)
+        {
+            value++;
+            color = new Color(1 - value / 25f, material.color.g, value/ 25f);
+            GetComponent<Renderer>().material.color = color;
+        }
+        print("B " + material.color.b);
+        print("R " + material.color.r);
+
         valueDisplayer.SetValue(value);
         if (value == 0)
         { 
@@ -29,6 +56,9 @@ public class Block : MonoBehaviour
         while (true)
         {
             value--;
+            color = new Color(1 - value / 25f, material.color.g, value / 25f);
+            GetComponent<Renderer>().material.color = color;
+
             snake.RemoveBone();
             yield return new WaitForSeconds(destroyingFrequency);
         }
