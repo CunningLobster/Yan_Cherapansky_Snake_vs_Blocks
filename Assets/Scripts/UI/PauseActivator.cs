872c9a565f1a5ae0c9abe279edced1e0211a97be
CunningLobster <cherepansky.yan@yandex.ru>
2021-12-23 22:50:42 +0300

    Probably fixed a bug

--- Assets/Scripts/UI/PauseActivator.cs
+++ Assets/Scripts/UI/PauseActivator.cs
@@ -7,8 +7,6 @@ namespace UI
     public class PauseActivator : MonoBehaviour
     {
         [SerializeField] PauseScreen pauseScreen;
-        public bool isPaused;
-
         public void OnEsc(InputAction.CallbackContext context)
         {
             pauseScreen.gameObject.SetActive(true);
