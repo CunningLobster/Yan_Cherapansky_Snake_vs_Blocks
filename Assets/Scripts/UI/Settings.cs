301b4a9d151e7af9e717904b76ee6a8f0b5d82c8
CunningLobster <cherepansky.yan@yandex.ru>
2021-12-23 05:40:21 +0300

    Removed sound activator

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -1,4 +1,4 @@
-using Control;// To Fix
+using Control;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,41 +9,19 @@ namespace UI
     public class Settings : MonoBehaviour
     {
         [SerializeField] Slider sensSlider;
-        [SerializeField] Toggle soundOn;
 
         InputProvider inputProvider;
-        public const string soundOnKey = "soundOn";
 
         private void Awake()
         {
             sensSlider.value = PlayerPrefs.GetFloat(InputProvider.sensKey);
             inputProvider = FindObjectOfType<InputProvider>();
-            soundOn.isOn = KeyToSoundBool(PlayerPrefs.GetInt(soundOnKey, 1));
         }
 
         private void Update()
         {
-            PlayerPrefs.SetInt(soundOnKey, SoundBoolToKey(soundOn.isOn));
-            Camera.main.GetComponent<AudioListener>().enabled = soundOn.isOn;
             PlayerPrefs.SetFloat(InputProvider.sensKey, sensSlider.value);
-
             Debug.Log(PlayerPrefs.GetFloat(InputProvider.sensKey));
-        }
-
-        int SoundBoolToKey(bool value)
-        {
-            if (value)
-                return 1;
-            else
-                return 0;
-        }
-
-        bool KeyToSoundBool(int value)
-        {
-            if (value == 1)
-                return true;
-            else
-                return false;
         }
     }
 }
